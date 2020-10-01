1763722cb8bd43dd2d986d20e3558bb31b582560
alongur123 <guralon2804@gmail.com>
2020-10-01 16:13:32 +0300

    remove

--- client/obligo-client/src/app/chat/chat.routing.ts
+++ client/obligo-client/src/app/chat/chat.routing.ts
@@ -1,10 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ChatComponent } from './containers/chat/chat.component';
-import { HomeComponent } from './containers/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'room/:room', component: ChatComponent },
+  { path: '', component: ChatComponent },
 ];
 
 export const ChatRoutes = RouterModule.forChild(routes);
